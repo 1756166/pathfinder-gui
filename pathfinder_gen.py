97deb454750b4f6c298484472f1b5dc34ace9674
Krupanidhi Peterson <krupanidhip@hotmail.com>
2019-09-17 20:32:19 -0400

    Added ability to delete waypoints

--- pathfinder_gen.py
+++ pathfinder_gen.py
@@ -13,12 +13,16 @@ dimensions = (background.get_width(), background.get_height())
 
 plus_button = pygame.image.load('Resources\\add_waypoint.jpg')
 plus_button = pygame.transform.scale(plus_button, (42, 42)) # NICE
+trash_can = pygame.image.load('Resources\\remove_waypoint.jpg')
+trash_can = pygame.transform.scale(trash_can, (42, 42)) # VERY NICE
+
 
 display = pygame.display.set_mode(dimensions)
 pygame.display.set_caption('Generate Waypoints')
 clock = pygame.time.Clock()
 
-button_rect = display.blit(plus_button, (0, 0))
+add_waypoint_rect = display.blit(plus_button, (0, 0))
+del_waypoint_rect = display.blit(trash_can, (background.get_width() - 42, 0))
 
 CRASHED = False
 
@@ -33,6 +37,7 @@ while not CRASHED:
 	clock.tick(60)
 	display.blit(background, (0, 0))
 	display.blit(plus_button, (0, 0))
+	display.blit(trash_can, (background.get_width() - 42, 0))
 	for waypnt, bounding_rect in waypoints_info:
 		waypnt.draw(display)
 
@@ -43,22 +48,27 @@ while not CRASHED:
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			mouse_rect = pygame.Rect(pygame.mouse.get_pos(), (1, 1))
 
-			if button_rect.contains(mouse_rect): # if the plus button is pressed
+			if add_waypoint_rect.contains(mouse_rect): # if the plus button is pressed
 				id += 1
 				waypoints_info.append((Waypoint(id, (init_x, init_y)), Waypoint(id, (init_x, init_y)).bounding_rect(display)))
 				init_y += 12
+			elif del_waypoint_rect.contains(mouse_rect):
+				if selected_waypoint is waypoints_info[-1]:
+					selected_waypoint = None
+				id -= 1
+				waypoints_info.pop(-1) # Deletes last elem in list of waypoints
 
 			for waypnt, bounding_rect in waypoints_info:
 				if bounding_rect.contains(mouse_rect):
-					print('contains')
-					selected_waypoint = waypoints_info.remove((waypnt, bounding_rect))
+					selected_waypoint = (waypnt, bounding_rect)
 
 		if event.type == pygame.MOUSEMOTION and event.buttons[0] == 1:
-			mouse_rect = pygame.Rect(pygame.mouse.get_pos(), (1, 1))
+			if selected_waypoint != None:
+				mouse_rect = pygame.Rect(pygame.mouse.get_pos(), (1, 1))
+				selected_waypoint[0].update_coords(pygame.mouse.get_pos())
 
-			if selected_waypoint[1].contains(mouse_rect):
-				pass
 		pygame.display.update()
+
 
 pygame.quit()
 quit()
